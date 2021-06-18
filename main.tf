9f79822d63686aac09970a7b7cdd330028543f11
sujith268 <78713091+sujith268@users.noreply.github.com>
2021-06-18 08:34:23 -0700

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0d8d212151031f51c"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "app_server_tf"
+  }
+}
